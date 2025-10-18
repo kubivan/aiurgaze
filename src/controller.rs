734256f2ee212f70c956fb33c5ee27342cbe31ed
kubivan <2262408+kubivan@users.noreply.github.com>
2025-10-18 22:37:09 +0300

    fix: add map layers
    * placement
    * pathing
    * creep

--- src/controller.rs
+++ src/controller.rs
@@ -1,14 +1,25 @@
 use bevy::asset::AssetServer;
 use bevy::color::Color;
 use sc2_proto::sc2api::Response_oneof_response::{game_info, observation};
-use bevy::prelude::{Commands, Res, ResMut};
+use bevy::prelude::{Commands, Res, ResMut, Resource, Query};
 use bevy_ecs_tilemap::prelude::{TileColor, TileStorage};
 use bevy_ecs_tilemap::tiles::TilePos;
 use bevy_tokio_tasks::TokioTasksRuntime;
 use crate::proxy_ws::{ProxyWS, ProxyWSResource};
-use crate::map::spawn_tilemap;
+use crate::map::{spawn_tilemap, TerrainLayers, TerrainLayer, blend_tile_color};
 use crate::entity_system::EntitySystem;
 use crate::units::{handle_observation, UnitIconAssets, UnitRegistry, UnitTypeIndex};
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+
+// Resource to store static terrain layers and tile storage
+#[derive(Resource)]
+pub struct MapResource {
+    pub static_layers: TerrainLayers,
+    pub tile_storage: TileStorage,
+    pub last_creep_hash: u64,
+    pub last_energy_hash: u64,
+}
 
 pub fn setup_proxy(mut commands: Commands, runtime: Res<TokioTasksRuntime>) {
     println!("======setup_proxy====");
@@ -25,37 +36,69 @@ pub fn setup_proxy(mut commands: Commands, runtime: Res<TokioTasksRuntime>) {
     });
 }
 
-fn recolor_tile(
-    commands: &mut Commands,
-    storage: &mut TileStorage,
-    pos: &TilePos,
-    color: Color
+
+fn calculate_layer_hash(layer: &Option<TerrainLayer>) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    if let Some(layer) = layer {
+        layer.data.hash(&mut hasher);
+    }
+    hasher.finish()
+}
+
+fn update_tilemap_colors(
+    tile_storage: &TileStorage,
+    static_layers: &TerrainLayers,
+    creep_layer: Option<&TerrainLayer>,
+    energy_layer: Option<&TerrainLayer>,
+    tile_color_query: &mut Query<&mut TileColor>,
 ) {
-    if let Some(tile_entity) = storage.get(&pos) {
-        commands.entity(tile_entity).insert(TileColor(color));
+    let (width, height) = static_layers.get_dimensions();
+
+    for y in 0..height {
+        for x in 0..width {
+            let tile_pos = TilePos { x, y };
+
+            if let Some(tile_entity) = tile_storage.get(&tile_pos) {
+                if let Ok(mut tile_color) = tile_color_query.get_mut(tile_entity) {
+                    // Get static layer values
+                    let pathing = static_layers.pathing.as_ref().map_or(0, |l| l.get_value(x, y));
+                    let placement = static_layers.placement.as_ref().map_or(0, |l| l.get_value(x, y));
+                    let height_val = static_layers.height.as_ref().map_or(128, |l| l.get_value(x, y));
+
+                    // Get dynamic layer values
+                    let creep = creep_layer.map_or(0, |l| l.get_value(x, y));
+                    let energy = energy_layer.map_or(0, |l| l.get_value(x, y));
+
+                    // Blend colors
+                    let color = blend_tile_color(pathing, placement, creep, energy, height_val);
+
+                    // Directly mutate the color component
+                    tile_color.0 = color;
+                }
+            }
+        }
     }
 }
 
 pub fn response_controller_system(
     proxy_res: Option<ResMut<ProxyWSResource>>,
+    mut map_res: Option<ResMut<MapResource>>,
     mut commands: Commands,
     mut asset_server: Res<AssetServer>,
     mut icon_assets: Res<UnitIconAssets>,
     mut registry: ResMut<UnitRegistry>,
     mut type_index: ResMut<UnitTypeIndex>,
     entity_system: Res<EntitySystem>,
+    mut tile_color_query: Query<&mut TileColor>,
 ) {
     let mut proxy_res = match proxy_res {
         Some(res) => res,
-        None => return, // Resource not available yet, skip system
+        None => return,
     };
 
     while let Ok(resp) = proxy_res.rx.try_recv() {
-        //println!("Got response: {:?}", resp);
-
         match resp.response.unwrap() {
-            observation (obs )  => {
-                //println!("Got observation: {:?}", obs.observation.unwrap().game_loop);
+            observation (obs)  => {
                 handle_observation(&mut commands,
                                    &asset_server,
                                    &icon_assets,
@@ -63,34 +106,80 @@ pub fn response_controller_system(
                                    &mut type_index,
                                    &entity_system,
                                    &obs);
+
+                // Update dynamic layers (creep, energy, visibility) only if changed
+                if let Some(ref mut map_res) = map_res {
+                    let obs_data = obs.observation.as_ref().unwrap();
+                    let raw_data = obs_data.raw_data.as_ref().unwrap();
+                    let map_state = raw_data.map_state.as_ref();
+
+                    let creep_layer = map_state.and_then(|ms| ms.creep.as_ref()).map(|creep_data| {
+                        TerrainLayer::from_image_data(creep_data, crate::map::TerrainLayerKind::Creep)
+                    });
+
+                    // TODO: Extract energy layer when available
+                    // let energy_layer = ...
+
+                    // Calculate hashes to check if layers changed
+                    let new_creep_hash = calculate_layer_hash(&creep_layer);
+                    let new_energy_hash = 0; // TODO: calculate when energy layer is available
+
+                    // Only update if something changed
+                    if new_creep_hash != map_res.last_creep_hash || new_energy_hash != map_res.last_energy_hash {
+                        // Update all tile colors with new dynamic data
+                        update_tilemap_colors(
+                            &map_res.tile_storage,
+                            &map_res.static_layers,
+                            creep_layer.as_ref(),
+                            None, // energy_layer
+                            &mut tile_color_query,
+                        );
+
+                        // Update hashes
+                        map_res.last_creep_hash = new_creep_hash;
+                        map_res.last_energy_hash = new_energy_hash;
+                    }
+                }
             }
             game_info (gi) =>  {
-                let &start_raw = &gi.start_raw.as_ref().unwrap();
-                let &start_pos = &start_raw.start_locations.get(0).unwrap();
-                let path_layer = crate::map::TerrainLayer::from_image_data(
-                    start_raw.placement_grid.get_ref(),
-                    crate::map::TerrainLayerKind::Pathing);
-                println!("Got game info: map size {} : {}", path_layer.width, path_layer.height);
+                let start_raw = gi.start_raw.as_ref().unwrap();
+                let start_pos = start_raw.start_locations.get(0).unwrap();
 
-                // 👇 draw the map
-                let mut tile_storage = spawn_tilemap(
+                // Create static layers
+                let path_layer = TerrainLayer::from_image_data(
+                    start_raw.pathing_grid.as_ref().unwrap(),
+                    crate::map::TerrainLayerKind::Pathing);
+                let placement_layer = TerrainLayer::from_image_data(
+                    start_raw.placement_grid.as_ref().unwrap(),
+                    crate::map::TerrainLayerKind::Placement);
+                let height_layer = TerrainLayer::from_image_data(
+                    start_raw.terrain_height.as_ref().unwrap(),
+                    crate::map::TerrainLayerKind::Height);
+
+                println!("Got game info: map size {} x {}", path_layer.width, path_layer.height);
+
+                // Build static layers container
+                let mut static_layers = TerrainLayers::new();
+                static_layers.add_layer(path_layer);
+                static_layers.add_layer(placement_layer);
+                static_layers.add_layer(height_layer);
+
+                // Spawn the tilemap with initial static layers only
+                let tile_storage = spawn_tilemap(
                     &mut commands,
-                    &path_layer,
+                    &static_layers,
                     &mut asset_server,
                 );
-                let blue = Color::srgba(0.0, 0.0, 1.0, 1.0);
-                let black = Color::srgba(0.0, 0.0, 0.0, 1.0);
-                recolor_tile(&mut commands,
-                             &mut tile_storage,
-                             &TilePos { x: start_pos.x.unwrap() as u32, y: start_pos.y.unwrap() as u32 },
-                             blue);
-                recolor_tile(&mut commands,
-                             &mut tile_storage,
-                             &TilePos { x: 0, y: 0 },
-                             black);
 
-                println!("Spawned tilemap entity, start pos : {:?}", start_pos);
+                // Store the static layers and tile storage as a resource
+                commands.insert_resource(MapResource {
+                    static_layers,
+                    tile_storage,
+                    last_creep_hash: 0,
+                    last_energy_hash: 0,
+                });
 
+                println!("Spawned tilemap, start pos: {:?}", start_pos);
             }
             _ => ()
         };
--- src/map.rs
+++ src/map.rs
@@ -1,31 +1,27 @@
-use bevy::asset::{Handle, RenderAssetUsages};
+use bevy::asset::Handle;
 use bevy::image::Image;
 use bevy::prelude::*;
-use bevy::render::render_resource::{Extent3d, TextureDimension, TextureFormat};
 use bevy_ecs_tilemap::prelude::*;
-use bevy_sprite::SliceScaleMode::Tile;
 use sc2_proto::common::ImageData;
-use sc2_proto::sc2api::{ResponseGameInfo, ResponseObservation};
-
 #[derive(PartialEq)]
 pub enum TerrainLayerKind {
     Pathing,
     Placement,
     Height,
+    Creep,
+    Energy,
 }
-
 pub struct TerrainLayer {
     kind: TerrainLayerKind,
     pub width: u32,
     pub height: u32,
-    data: Vec<u8>, // raw bytes from ImageData.data
+    pub data: Vec<u8>, // raw bytes from ImageData.data - made public for hashing
 }
-
 fn unpack_bits(bytes: &[u8]) -> Vec<bool> {
     let mut bits = Vec::with_capacity(bytes.len() * 8);
     for byte in bytes {
         for i in (0..8).rev() {
-            bits.push((byte >> i) & 1 == 1);
+            bits.push((byte >> (i as u8)) & 1 == 1);
         }
     }
     bits
@@ -38,7 +34,6 @@ impl TerrainLayer {
             .iter()
             .map(|&b| if b { 255u8 } else { 0 })
             .collect();
-
         TerrainLayer {
             kind,
             width,
@@ -56,22 +51,18 @@ impl TerrainLayer {
         } else {
             Self::from_image_data8(img, kind)
         }
-
     }
     pub fn from_image_data8(img: &ImageData, kind: TerrainLayerKind) -> Self {
         let bits = img.bits_per_pixel.unwrap();
         assert_eq!(bits, 8, "Only 8 bits per pixel supported for now (got {bits})");
-
         let img_size = img.size.clone().unwrap();
         let width = img_size.x.unwrap().clone() as u32;
         let height = img_size.y.unwrap().clone() as u32;
-
         assert_eq!(
             img.data.as_ref().unwrap().len(),
             (width * height) as usize,
             "Image data size mismatch"
         );
-
         Self {
             kind,
             width,
@@ -79,55 +70,139 @@ impl TerrainLayer {
             data: img.data.clone().unwrap(),
         }
     }
+    pub fn get_value(&self, x: u32, y: u32) -> u8 {
+        let idx = (y * self.width + x) as usize;
+        if idx < self.data.len() {
+            self.data[idx]
+        } else {
+            0
+        }
+    }
 }
-
+/// Get tile color based on terrain properties - Dark IDE theme
+/// Creep overrides all other colors with purple
+pub fn blend_tile_color(
+    pathing: u8,
+    placement: u8,
+    creep: u8,
+    energy: u8,
+    height: u8,
+) -> Color {
+    let intensity = 0.5 + (height as f32 / 255.0) * 0.5;
+    // Creep overrides everything with purple (like error highlighting in IDE)
+    if creep > 0 {
+        return Color::srgb(0.4 * intensity, 0.1 * intensity, 0.5 * intensity);
+    }
+    // Energy overrides with cyan/blue (like info highlighting)
+    if energy > 0 {
+        let intensity = 0.5 + (height as f32 / 255.0) * 0.5;
+        return Color::srgb(0.1 * intensity, 0.3 * intensity, 0.6 * intensity);
+    }
+    // Discrete colors for pathable/placeable combinations (dark IDE theme)
+    let base_color = match (pathing > 0, placement > 0) {
+        (false, false) => Color::srgb(0.05, 0.05, 0.05),  // Non-pathable, non-placeable: Almost black #0D0D0D
+        (true, false)  => Color::srgb(0.12, 0.12, 0.13),  // Pathable only: Dark grey #1E1E21 (like IDE background)
+        (false, true)  => Color::srgb(0.18, 0.18, 0.20),  // Placeable only (rare): Medium-dark grey #2E2E33
+        (true, true)   => Color::srgb(0.22, 0.22, 0.24),  // Both pathable & placeable: Light grey #383840 (like selected line)
+    };
+    // Apply height as brightness multiplier
+    let intensity = 0.6 + (height as f32 / 255.0) * 0.4;
+    Color::srgb(
+        base_color.to_srgba().red * intensity,
+        base_color.to_srgba().green * intensity,
+        base_color.to_srgba().blue * intensity,
+    )
+}
+pub struct TerrainLayers {
+    pub pathing: Option<TerrainLayer>,
+    pub placement: Option<TerrainLayer>,
+    pub height: Option<TerrainLayer>,
+    pub creep: Option<TerrainLayer>,
+    pub energy: Option<TerrainLayer>,
+}
+impl TerrainLayers {
+    pub fn new() -> Self {
+        Self {
+            pathing: None,
+            placement: None,
+            height: None,
+            creep: None,
+            energy: None,
+        }
+    }
+    pub fn add_layer(&mut self, layer: TerrainLayer) {
+        match layer.kind {
+            TerrainLayerKind::Pathing => self.pathing = Some(layer),
+            TerrainLayerKind::Placement => self.placement = Some(layer),
+            TerrainLayerKind::Height => self.height = Some(layer),
+            TerrainLayerKind::Creep => self.creep = Some(layer),
+            TerrainLayerKind::Energy => self.energy = Some(layer),
+        }
+    }
+    pub fn get_dimensions(&self) -> (u32, u32) {
+        // Get dimensions from the first available layer
+        if let Some(ref layer) = self.pathing {
+            return (layer.width, layer.height);
+        }
+        if let Some(ref layer) = self.placement {
+            return (layer.width, layer.height);
+        }
+        if let Some(ref layer) = self.height {
+            return (layer.width, layer.height);
+        }
+        if let Some(ref layer) = self.creep {
+            return (layer.width, layer.height);
+        }
+        if let Some(ref layer) = self.energy {
+            return (layer.width, layer.height);
+        }
+        (0, 0)
+    }
+}
 pub fn spawn_tilemap(
     commands: &mut Commands,
-    layer: &TerrainLayer,
+    layers: &TerrainLayers,
     asset_server: &mut Res<AssetServer>,
     #[cfg(all(not(feature = "atlas"), feature = "render"))] array_texture_loader: Res<
         ArrayTextureLoader,
     >,
 ) -> TileStorage {
-
     let texture_handle: Handle<Image> = asset_server.load("tiles.png");
-
+    let (width, height) = layers.get_dimensions();
     let map_size = TilemapSize {
-        x: layer.width,
-        y: layer.height,
+        x: width,
+        y: height,
     };
-    //let map_size = TilemapSize { x: 32, y: 32 };
-
     let tilemap_entity = commands.spawn_empty().id();
-
     let mut tile_storage = TileStorage::empty(map_size);
-
-    // Fill map tiles
-    for y in 0..layer.height - 1 {
-        for x in 0..layer.width -1 {
+    // Fill map tiles with blended colors
+    for y in 0..height {
+        for x in 0..width {
             let tile_pos = TilePos { x, y };
-
-            let idx = (y * layer.width + x) as usize;
-            let val = layer.data[idx];
-            let texture_index = if val == 0 { 4 } else { 5 };
-
+            // Get values from each layer
+            let pathing = layers.pathing.as_ref().map_or(0, |l| l.get_value(x, y));
+            let placement = layers.placement.as_ref().map_or(0, |l| l.get_value(x, y));
+            let height_val = layers.height.as_ref().map_or(128, |l| l.get_value(x, y));
+            let creep = layers.creep.as_ref().map_or(0, |l| l.get_value(x, y));
+            let energy = layers.energy.as_ref().map_or(0, |l| l.get_value(x, y));
+            // Get color based on all layers
+            let color = blend_tile_color(pathing, placement, creep, energy, height_val);
             let tile_entity = commands
                 .spawn(TileBundle {
                     position: tile_pos,
+                    visible: TileVisible(true),
                     tilemap_id: TilemapId(tilemap_entity),
-                    // color: TileColor(color),
-                    texture_index: TileTextureIndex(texture_index),
+                    color: TileColor(color),
+                    texture_index: TileTextureIndex(5), // Use a single white tile for coloring
                     ..Default::default()
                 })
                 .id();
             tile_storage.set(&tile_pos, tile_entity);
         }
     }
-
     let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
     let grid_size = tile_size.into();
     let map_type = TilemapType::default();
-
     commands.entity(tilemap_entity).insert(TilemapBundle {
         grid_size,
         map_type,
@@ -139,7 +214,6 @@ pub fn spawn_tilemap(
         transform: Transform::from_xyz(0.0, 0.0, 0.0), //z = 0.0 (background)
         ..Default::default()
     });
-
     // Add atlas to array texture loader so it's preprocessed before we need to use it.
     // Only used when the atlas feature is off and we are using array textures.
     #[cfg(all(not(feature = "atlas"), feature = "render"))]
@@ -152,4 +226,3 @@ pub fn spawn_tilemap(
     }
     return tile_storage;
 }
-
